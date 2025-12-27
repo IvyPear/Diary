0bdbd129390f37dff67e4595e6d400a6ebed52c0
Quynh Muy <my.nguyen230934@vnuk.edu.vn>
2025-12-27 17:38:24 +0700

    Use Vite + Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: './',  // Dùng './' cho local, đổi thành '/Diary/' khi deploy Project Pages
-  plugins: [
-    tailwindcss(),
-  ],
+  base: './',
 });
